a64ebb8663bb6e5dd1b2fd60b67dc057a9b06d60
sylvestertarnowski <34459277+sylvestertarnowski@users.noreply.github.com>
2019-04-30 14:05:18 +0200

    State type defined, basic stats of Monster intialised

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+type State = {
+  name: string;
+  performingAction: boolean;
+  action: string;
+  happiness: number;
+  power: number;
+  hunger: number;
+};
+
+class App extends React.Component {
+  readonly state: State = {
+    name: "Unknown",
+    performingAction: false,
+    action: "none",
+    happiness: 50,
+    power: 20,
+    hunger: 0,
+  }
+
+  handleFeed = () => {
+    this.setState(prevState => {
+      
+    })
+  }
+
+  render() {
+    const { name, happiness, power } = this.state;
+
+    return(
+      <div className="App">
+
+        <div>{`The Monster name is ${name}, and its happiness level is ${happiness}. Current Power Level: ${power}`}</div>
+        <button
+          id="feed"
+          onClick={this.handleFeed}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Feed
+        </button>
+      </div>
+    )
+  }
 }
 
 export default App;
